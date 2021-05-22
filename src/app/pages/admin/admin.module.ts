765dc349f8115a9e192e02847b93e974fb1bd8e6
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-05-22 21:35:12 +0200

    delete unnecessary components

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -3,16 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { PostFormComponent } from './post-form/post-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PostListComponent } from './post-list/post-list.component';
-import { PostListElementComponent } from './post-list-element/post-list-element.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    PostFormComponent,
-    PostListComponent,
-    PostListElementComponent
+    PostFormComponent
   ],
   imports: [
     CommonModule,
